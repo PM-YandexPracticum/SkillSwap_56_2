f3fcb1f96a8979f46c7e6e27e5357f6084ca0cc9
Pavel <Pavel01995@yandex.kz>
2026-09-04 13:30:02 +0500

    fixe some mistakes

--- src/pages/CatalogPage/index.tsx
+++ src/pages/CatalogPage/index.tsx
@@ -1,10 +1,29 @@
-// TODO: реализовать страницу CatalogPage
+import { useState } from 'react' // 1. Импортируем useState
+import { Input } from '../../shared/ui/Input/input'
+import styles from '../../shared/ui/Input/Input.module.css'
 
-export default function CatalogPage() {
+export default function RegisterStep3() {
+  // 2. Создаем стейт для хранения значения
+  const [email, setEmail] = useState('')
+
   return (
-    <main>
-      <h1>CatalogPage</h1>
-      <p>Страница в разработке</p>
-    </main>
+    <div className={styles.wrapper}>
+      <div className={styles.card}>
+        <div className={styles.form}>
+          {/* 3. Передаем value и onChange в компонент */}
+          <Input
+            label="Email"
+            placeholder="Введите ваш email"
+            value={email}
+            onChange={(value) => setEmail(value)}
+            className={styles.fullInputRegister}
+            error="Пожалуйста, введите действительный email"
+          />
+
+          {/* Можно вывести для проверки прямо на странице */}
+          <p>Введенный email: {email}</p>
+        </div>
+      </div>
+    </div>
   )
 }
