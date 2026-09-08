3f5396ffd96102dd784bac6c6ffa980f442e098a
liya <l.skripilina@yandex.ru>
2026-09-09 00:09:39 +0700

    return file config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('jest').Config} */
 module.exports = {
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: [require.resolve('@testing-library/jest-dom')],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   roots: ['<rootDir>/src'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   transform: {
