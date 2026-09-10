527c87205ed9d162bef0efae9a7e79f61035d327
Denis Lukash <denis_lukash@inbox.ru>
2026-09-10 19:35:35 +0300

    feat(pages): add 404 and 500 error pages

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -2,13 +2,9 @@ import { ErrorState } from '@/shared/ui/ErrorState'
 import errorImage from './assets/error404.png'
 
 export default function NotFoundPage() {
-  const handleReport = () => {
-    console.log('Сообщение об ошибке')
-  }
-
   return (
     <main>
-      <ErrorState onReport={handleReport} illustration={errorImage} title='Страница не найдена' description='К сожалению, эта страница недоступна. Вернитесь на главную страницу или попробуйте позже'/>
+      <ErrorState illustration={errorImage} title='Страница не найдена' description='К сожалению, эта страница недоступна. Вернитесь на главную страницу или попробуйте позже'/>
     </main>
   )
 }
--- src/pages/ServerErrorPage/index.tsx
+++ src/pages/ServerErrorPage/index.tsx
@@ -2,13 +2,9 @@ import { ErrorState } from '@/shared/ui/ErrorState'
 import errorImage from './assets/error500.png'
 
 export default function ServerErrorPage() {
-  const handleReport = () => {
-    console.log('Сообщение об ошибке')
-  }
-
   return (
     <main>
-      <ErrorState onReport={handleReport} illustration={errorImage} title='На сервере произошла ошибка' description='Попробуйте позже или вернитесь на главную страницу'/>
+      <ErrorState illustration={errorImage} title='На сервере произошла ошибка' description='Попробуйте позже или вернитесь на главную страницу'/>
     </main>
   )
 }
