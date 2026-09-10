beb5e925f4430c7aa149f222daa5f67717dc24d5
darkperseus <sashamix123456@gmail.com>
2026-09-10 19:42:25 +0300

    LikedIcon

--- src/shared/ui/ButtonLike/ButtonLike.tsx
+++ src/shared/ui/ButtonLike/ButtonLike.tsx
@@ -1,4 +1,5 @@
 import styles from './ButtonLike.module.css'
+import { LikeIcon } from "@/shared/ui/icons/LikeIcons"
 type ButtonLikeProps = {
   isLiked: boolean
   onClick: () => void
@@ -7,9 +8,7 @@ type ButtonLikeProps = {
 export const ButtonLike = ({ isLiked, onClick }: ButtonLikeProps) => {
   return (
     <button className={`${styles.button} ${isLiked ? styles.liked : ''}`} type="button" onClick={onClick}>
-      <svg className={styles.icon} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path className={styles.path} d="M7.95 4C5.21619 4 3 6.1521 3 8.80682C3 13.6136 8.85 17.9835 12 19C15.15 17.9835 21 13.6136 21 8.80682C21 6.1521 18.7838 4 16.05 4C14.3759 4 12.8958 4.80707 12 6.04238C11.1042 4.80707 9.62414 4 7.95 4Z" fill="currentColor" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-      </svg>
+      <LikeIcon className={styles.icon}></LikeIcon>
     </button>
   )
 }
