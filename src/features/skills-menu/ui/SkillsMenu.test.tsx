46a3035cf29010a412af117e9ff2f2a7e060c698
Pavel <Pavel01995@yandex.kz>
2026-09-13 16:56:31 +0500

    Компонет открытия меню навыков

--- src/features/skills-menu/ui/SkillsMenu.test.tsx
+++ src/features/skills-menu/ui/SkillsMenu.test.tsx
@@ -11,50 +11,43 @@ jest.mock('@/shared/ui/icons/assets/home.svg', () => 'home-stub')
 jest.mock('@/shared/ui/icons/assets/lifestyle.svg', () => 'lifestyle-stub')
 
 test('клик кнопки открытия меню скиллов', () => {
-render(<SkillsMenu/>)
+  render(<SkillsMenu />)
 
-const button =screen.getByText('Все навыки')
-const menu =screen.getByTestId('menu')
+  const button = screen.getByText('Все навыки')
+  const menu = screen.getByTestId('menu')
 
-expect(menu.className).not.toContain('open') 
+  expect(menu.className).not.toContain('open')
 
-fireEvent.click(button)
-expect(menu.className).toContain('open')  // Проверяем состояние а не  появление окна так как из-за стилей окно уже хранится  в открытом состояни для анимации ccs
-
-
+  fireEvent.click(button)
+  expect(menu.className).toContain('open') // Проверяем состояние а не  появление окна так как из-за стилей окно уже хранится  в открытом состояни для анимации ccs
 })
 
-test(' закрытие вне окна' ,()=>{
-render(<SkillsMenu/>)
+test(' закрытие вне окна', () => {
+  render(<SkillsMenu />)
 
-const button =screen.getByText('Все навыки')
-const menu =screen.getByTestId('menu')
+  const button = screen.getByText('Все навыки')
+  const menu = screen.getByTestId('menu')
 
-expect(menu.className).not.toContain('open') 
+  expect(menu.className).not.toContain('open')
 
-fireEvent.click(button)
-expect(menu.className).toContain('open')
+  fireEvent.click(button)
+  expect(menu.className).toContain('open')
 
-fireEvent.mouseDown(document.body)
-expect(menu.className).not.toContain('open')
-
-
-
+  fireEvent.mouseDown(document.body)
+  expect(menu.className).not.toContain('open')
 })
 
+test('закрытие через esc', () => {
+  render(<SkillsMenu />)
 
-test('закрытие через esc', ()=>{
-render(<SkillsMenu />)
+  const button = screen.getByText('Все навыки')
+  const menu = screen.getByTestId('menu')
 
-const button = screen.getByText('Все навыки')
-const menu = screen.getByTestId('menu')
+  expect(menu.className).not.toContain('open')
 
-expect(menu.className).not.toContain('open')
+  fireEvent.click(button)
+  expect(menu.className).toContain('open')
 
-fireEvent.click(button)
-expect(menu.className).toContain('open')
-
-fireEvent.keyDown(button,{key:'Escape'})
-expect(menu.className).not.toContain('open')
-
+  fireEvent.keyDown(button, { key: 'Escape' })
+  expect(menu.className).not.toContain('open')
 })
